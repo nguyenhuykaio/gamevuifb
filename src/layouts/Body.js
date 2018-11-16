c1a719f34d7dc7700d93d2835d170e8cbccba9ab
nguyenhuykaio <nguyenhuykaio@gmail.com>
2018-11-16 11:05:23 +0700

    fix sever

--- src/layouts/Body.js
+++ src/layouts/Body.js
@@ -3,7 +3,7 @@ import { importAll } from '../common/common';
 import { HOST_WEB, HOST_IMG } from '../component/global';
 
 
-const HOST_IMAGE = HOST_IMG+'/game/';
+const HOST_IMAGE = HOST_IMG + '/game/';
 const images = importAll(require.context('../img', false, /\.(png|jpe?g|svg)$/));
 
 
@@ -14,70 +14,46 @@ export class Body extends Component {
         this.state = {
             listData: []
         };
-        // this.loadData();
-        // this.loadApi();
+
     }
-    // loadData() {
-    //     fetch("http://5b12493ad50a5c0014ef11c3.mockapi.io/test")
-    //         .then(res => {
-    //             return res.json()
-    //         }).then(data => {
-    //             // console.log(data);
-    //             this.setState({
-    //                 listData: data
-    //             });
-    //         })
-    //         // .then(data => console.log(data))
-    //         .catch(err => console.log(err))
-    // }
+
     componentDidMount() {
         this.loadApi();
     }
-    // render() {
-    //     return (
-    //         <div className="frame-body">
-    //             {this.state.listData.map(item => (         
-    //                 <li key={item.name}>
-    //                 {item.name} -{item.avatar}-{item.createdAt}
-    //                 </li>                    
-    //             ))}
-    //         </div>
-    //     );
-    // }
+
     loadApi() {
-        fetch(HOST_WEB+"allgame")
+        fetch(HOST_WEB + "allgame")
             .then(res => {
                 return res.json()
             }).then(data => {
-                // console.log(data);
+
                 this.setState({
                     listData: data
                 });
             })
-            // .then(data => console.log(data))
             .catch(err => console.log(err))
     }
 
     render() {
         return (
             //    start frame body of game 
-                <div className="frame-body">
-                    <div className="frame-game">
-                        {
-                            this.state.listData.map(item => (
+            <div className="frame-body">
+                <div className="frame-game">
+                    {
+                        this.state.listData.map(item => (
 
-                                // {/* start 1 game */ }
-                                <a href={"/game"+item.id} className="layout-game" key={item.name}>
-                                    <img className="img-game"  src={HOST_IMAGE +item.picture}  />
-                                    <h3 className="name-game">{item.name}</h3>
-                                </a>
-                                // {/* end 1 game */ }
+                            // {/* start 1 game */ }
+                            <a href={"/game" + item.id} className="layout-game" key={item.name}>
+                                <img className="img-game" src={HOST_IMAGE + item.picture} />
+                                <h3 className="name-game">{item.name}</h3>
+                            </a>
+                            // {/* end 1 game */ }
 
-                            ))
-                        }
+                        ))
+                    }
 
-                    </div>
-                </div>        
+                </div>
+            </div>
             // end frame body of game 
         );
     }
